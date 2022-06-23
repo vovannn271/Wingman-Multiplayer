1035fe02f11a106c8c90b52f89f4156d5fa0dfad
vovannn271 <vovannn271@gmail.com>
2022-06-24 00:01:37 +0200

    projectiles working with warnings

--- Assets/_Scripts/Skills/ChangeGravitySkill.cs
+++ Assets/_Scripts/Skills/ChangeGravitySkill.cs
@@ -5,6 +5,7 @@ using Opsive.UltimateCharacterController.Character.Abilities;
 public class ChangeGravitySkill : Ability
 {
     private float sizeOfCharacter = 2f;
+    private AlignToGravityZone m_AlignToGravityZone;
     public override bool CanStartAbility()
     {
         // An attribute may prevent the ability from starting.
@@ -16,17 +17,33 @@ public class ChangeGravitySkill : Ability
         return m_CharacterLocomotion != null;
     }
 
+    public override void Start()
+    {
+        for (int i = 0; i < m_CharacterLocomotion.Abilities.Length; ++i)
+        {
+                // The MoveTowards and ItemToggleAbilityBlock abilities are a special type of ability in that it is started by the controller.
+                if (m_CharacterLocomotion.Abilities[i] is AlignToGravityZone)
+                {
+                    m_AlignToGravityZone = m_CharacterLocomotion.Abilities[i] as AlignToGravityZone;
+                }
+            }
+    }
+
+
+
    public override bool IsConcurrent { get { return true; } }
 
     protected override void AbilityStarted()
     {
         base.AbilityStarted();
-        
-        //to stop teleporting through textures
-        Vector3 tPos = m_Transform.position;
-        m_Transform.position = new Vector3( tPos.x, tPos.y + sizeOfCharacter, tPos.z );
 
+        //to stop teleporting through textures
+        // Vector3 tPos = m_Transform.position;
+        // m_Transform.position = new Vector3( tPos.x, tPos.y + sizeOfCharacter, tPos.z );
         m_CharacterLocomotion.GravityDirection *= -1;
+
+        
+
     }
 
     protected override void AbilityStopped( bool force )
