609ba74118ded66d4d917eaa75cda55e87b59a08
Volodymyr Komar <volodymyr.komar@comarch.com>
2022-07-07 00:12:24 +0200

    created empty phase shift skill

---
+++ Assets/_Scripts/Skills/PhaseShiftSkill.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Opsive.UltimateCharacterController.Character.Abilities;
+
+public class PhaseShiftSkill : Ability
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
