b3857278c93fc1e2653d90264319d9870864a95b
Volodymyr Komar <volodymyr.komar@comarch.com>
2022-07-07 16:04:52 +0200

    testing of phase shift ability

--- Assets/_Scripts/Skills/PhaseShiftSkill.cs
+++ Assets/_Scripts/Skills/PhaseShiftSkill.cs
@@ -5,15 +5,64 @@ using Opsive.UltimateCharacterController.Character.Abilities;
 
 public class PhaseShiftSkill : Ability
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject particlesOnShift;
+    [SerializeField] private float cooldown = 3f;
+    [SerializeField] private float durationTime = 3f;
+
+    
+    private float startTime;
+    private bool collisionLayerEnabled;
+    private GameObject currentParticlesObject;
+
+    public override bool CanStartAbility()
     {
-        
+        if (!base.CanStartAbility())
+        {
+            return false;
+        }
+
+        //Add coldown
+  
+        return true;
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void Update()
     {
+        base.Update();
+
+        if (!this.IsActive)
+        {
+            return;
+        }     
         
+        if (Time.time > startTime + durationTime)
+            {
+                Debug.Log( "stopping" );
+                StopAbility();
+
+         }
+        
+       
+
     }
+    protected override void AbilityStarted()
+    {
+        startTime = Time.time;
+        base.AbilityStarted();
+        currentParticlesObject = GameObject.Instantiate( particlesOnShift, this.m_GameObject.transform );
+        
+            
+        collisionLayerEnabled = m_CharacterLocomotion.CollisionLayerEnabled;
+        m_CharacterLocomotion.EnableColliderCollisionLayer( false );
+    }
+
+    protected override void AbilityStopped( bool force )
+    {
+        base.AbilityStopped( force );
+        m_CharacterLocomotion.EnableColliderCollisionLayer( collisionLayerEnabled );
+        GameObject.Destroy( currentParticlesObject );
+    }
+
+
+
 }
