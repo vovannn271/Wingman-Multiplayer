2a0c18860423b69c1f6f4120df0ecc46757b3de5
vovannn271 <vovannn271@gmail.com>
2022-07-08 12:56:01 +0200

    Working phase shift no vfx

--- Assets/_Scripts/Skills/PhaseShiftSkill.cs
+++ Assets/_Scripts/Skills/PhaseShiftSkill.cs
@@ -2,12 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Opsive.UltimateCharacterController.Character.Abilities;
-
+using Opsive.UltimateCharacterController.Traits;
 public class PhaseShiftSkill : Ability
 {
     [SerializeField] private GameObject particlesOnShift;
     [SerializeField] private float cooldown = 3f;
     [SerializeField] private float durationTime = 3f;
+    private CharacterHealth characterHealth;
+
+    public override void Awake()
+    {
+        base.Awake();
+        characterHealth = this.GetComponent<CharacterHealth>();
+    }
 
     
     private float startTime;
@@ -50,16 +57,18 @@ public class PhaseShiftSkill : Ability
         startTime = Time.time;
         base.AbilityStarted();
         currentParticlesObject = GameObject.Instantiate( particlesOnShift, this.m_GameObject.transform );
-        
-            
-        collisionLayerEnabled = m_CharacterLocomotion.CollisionLayerEnabled;
-        m_CharacterLocomotion.EnableColliderCollisionLayer( false );
+
+        characterHealth.Invincible = true;
+       // collisionLayerEnabled = m_CharacterLocomotion.CollisionLayerEnabled;
+        //m_CharacterLocomotion.EnableColliderCollisionLayer( false );
     }
 
     protected override void AbilityStopped( bool force )
     {
         base.AbilityStopped( force );
-        m_CharacterLocomotion.EnableColliderCollisionLayer( collisionLayerEnabled );
+        //m_CharacterLocomotion.EnableColliderCollisionLayer( collisionLayerEnabled );
+        characterHealth.Invincible = false;
+
         GameObject.Destroy( currentParticlesObject );
     }
 
