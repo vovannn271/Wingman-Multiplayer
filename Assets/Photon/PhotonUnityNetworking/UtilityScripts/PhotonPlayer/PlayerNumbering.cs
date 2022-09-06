3115d8798ebded9f36ff65a49b0aae739daf96e0
vovannn271 <vovannn271@gmail.com>
2022-09-06 14:31:54 +0200

    added bullet UI not finished

--- Assets/Photon/PhotonUnityNetworking/UtilityScripts/PhotonPlayer/PlayerNumbering.cs
+++ Assets/Photon/PhotonUnityNetworking/UtilityScripts/PhotonPlayer/PlayerNumbering.cs
@@ -259,7 +259,6 @@ namespace Photon.Pun.UtilityScripts
             int current = player.GetPlayerNumber();
             if (current != playerNumber)
             {
-				Debug.Log("PlayerNumbering: Set number "+playerNumber);
                 player.SetCustomProperties(new Hashtable() { { PlayerNumbering.RoomPlayerIndexedProp, (byte)playerNumber } });
             }
         }
