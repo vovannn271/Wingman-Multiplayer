e80a61570b366040c6a3648971a51c32ecbad9f2
Volodymyr Komar <vovannn271@gmail.com>
2022-09-30 09:09:11 +0200

    Added Offline mode

---
+++ Assets/_Scripts/Multiplayer/OfflineMode.cs
@@ -0,0 +1,19 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OfflineMode : MonoBehaviour
+{
+    private void Awake()
+    {
+        if (Application.isEditor)
+        {
+            gameObject.SetActive( false );
+        }
+    }
+    public void TurnOnOfflineMode()
+    {
+        PhotonNetwork.OfflineMode = true;
+    }
+}
